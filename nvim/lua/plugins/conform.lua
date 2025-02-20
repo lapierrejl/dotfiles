ea40b1b821f58bc2093eeb7342a73cd41d9d0203
Jeremiah LaPierre <jeremiah@writer.com>
2025-02-20 17:52:46 -0600

    adding additonal plugins and config

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+local util = require("conform.util")
+return {
+  "stevearc/conform.nvim",
+  opts = function()
+    ---@type conform.setupOpts
+    local opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_format = "fallback", -- not recommended to change
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        python = { "black" },
+        yaml = { "yamlfmt" },
+        -- rust = { "rustfmt" },
+      },
+    }
+    return opts
+  end,
+}
